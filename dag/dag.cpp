c983c0c48577cde1cd7982a66b4050339e313a54
Yutian Li <kevinleeone@gmail.com>
2014-05-27 15:55:29 +0800

    [dag] Add implementation for dag

---
+++ dag/dag.cpp
@@ -0,0 +1,28 @@
+#include "dag.h"
+#include "dag_node.h"
+#include <cstdint>
+
+using namespace std;
+
+uint64_t Dag::indexCounter = 0;
+
+Dag::Dag() {
+}
+
+Dag::~Dag() {
+}
+
+DataNode* Dag::NewDataNode() {
+    DataNode* ret = new DataNode;
+    ret->nodeID = indexCounter++;
+    indexToNode.insert(pair<uint64_t, DagNode*>(ret->nodeID, ret));
+    return ret;
+}
+
+OpNode* Dag::NewOpNode() {
+    OpNode* ret = new OpNode;
+    ret->nodeID = indexCounter++;
+    indexToNode.insert(pair<uint64_t, DagNode*>(ret->nodeID, ret));
+    return ret;
+}
+
