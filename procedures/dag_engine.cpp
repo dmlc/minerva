e3c051f9634bbc349d495644a157dacf167af533
Yutian Li <kevinleeone@gmail.com>
2014-05-30 16:49:15 +0800

    [dag] Find nodes from bottom to top

--- procedures/dag_engine.cpp
+++ procedures/dag_engine.cpp
@@ -2,27 +2,16 @@
 #include "dag/dag.h"
 #include "dag/dag_node.h"
 #include <map>
+#include <queue>
 
 using namespace std; 
 
 namespace minerva {
 
-// void DagEngine::Process(Dag& dag, vector<uint64_t> targets) {
-//   // TODO
-//   // 1. Create meta info
-//   // 2. Colorize
-//   ParseDagState(dag);
-//   map<uint64_t, NodeState>::iterator it;
-//   for (auto i: targets) {
-//     it = node_states_.find(i);
-//     if (it == node_states_.end()) { // Node not found
-//       continue;
-//     }
-//     it->second.state = kReady;
-//     if (dag.GetNode(i)) {
-//     }
-//   }
-// }
+void DagEngine::Process(Dag& dag, vector<uint64_t>& targets) {
+  ParseDagState(dag);
+  FindRootNodes(dag, targets);
+}
 
 void DagEngine::ParseDagState(Dag& dag) {
   // Create NodeState for new nodes. Note that only new nodes are inserted.
@@ -30,6 +19,33 @@ void DagEngine::ParseDagState(Dag& dag) {
     NodeState n;
     n.state = NodeState::kNoNeed;
     node_states_.insert(make_pair(i.first, n));
+  }
+}
+
+void DagEngine::FindRootNodes(Dag& dag, vector<uint64_t>& targets) {
+  queue<uint64_t> ready_node_queue;
+  for (auto i: targets) {
+    auto it = node_states_.find(i);
+    if (it == node_states_.end()) { // Node not found
+      continue;
+    }
+    ready_node_queue.push(i);
+  }
+  while (!ready_node_queue.empty()) {
+    uint64_t cur = ready_node_queue.front();
+    ready_node_queue.pop();
+    auto it = node_states_.find(cur);
+    auto node = dag.index_to_node_[cur];
+    it->second.state = NodeState::kReady; // Set state to ready
+    it->second.dependency_counter = node->predecessors_.size();
+    if (node->predecessors_.empty()) {
+      // Add to execution queue
+    } else {
+      // Traverse predecessors
+      for (auto i: node->predecessors_) {
+        ready_node_queue.push(i->node_id_);
+      }
+    }
   }
 }
 
--- procedures/dag_engine.h
+++ procedures/dag_engine.h
@@ -1,8 +1,10 @@
 #pragma once
 #include "procedures/dag_procedure.h"
 #include "common/common.h"
+#include "common/concurrent_blocking_queue.h"
 #include <cstdint>
 #include <map>
+#include <vector>
 
 namespace minerva {
 
@@ -11,19 +13,20 @@ struct NodeState {
     kNoNeed,
     kReady
   } state;
+  size_t dependency_counter;
 };
 
 class DagEngine : public DagProcedure {
  public:
-  void Process(Dag&);
+  void Process(Dag&, std::vector<uint64_t>&);
 
  private:
   DISALLOW_COPY_AND_ASSIGN(DagEngine);
   void ParseDagState(Dag&);
+  void FindRootNodes(Dag&, std::vector<uint64_t>&);
   std::map<uint64_t, NodeState> node_states_;
   // TODO private members including but not limited to
   // 1. Threadpool
-  // 2. Execution state (like counter)
 };
 
 }
