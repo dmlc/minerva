b826088e5f8c5364b15562a45b0349ca85c0d817
Yutian Li <kevinleeone@gmail.com>
2014-08-06 17:20:30 +0800

    [reduction-test] WIP. Need to implement reduction on NArray and test it.

--- tests/unit_test/reduction.cpp
+++ tests/unit_test/reduction.cpp
@@ -4,18 +4,6 @@
 using namespace minerva;
 using namespace std;
 
-void LoadAndOutput() {
-  SimpleFileLoader loader;
-  NArray na = NArray::LoadFromFile({4, 6}, "randmat.dat", &loader, {2, 2});
-
-  FileFormat format;
-  format.binary = false;
-  na.ToFile("randmat1.txt", format);
-
-  cout << "physical dag: " << endl;
-  cout << MinervaSystem::Instance().physical_dag().PrintDag() << endl;
-}
-
 void Test1() {
   Scale size{5, 3};
   float* data = new float[size.Prod()];
@@ -34,3 +22,4 @@ int main(int argc, char** argv) {
   Test1();
   return 0;
 }
+
