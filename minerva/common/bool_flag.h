5d3b1b0297a87a9582c72a3312b741f5248fdb95
Minjie Wang <wmjlyjemaine@gmail.com>
2014-08-06 17:09:15 +0800

    remove bool_flag.cpp

--- minerva/common/bool_flag.h
+++ minerva/common/bool_flag.h
@@ -4,14 +4,13 @@
 
 class BoolFlag {
  public:
-  BoolFlag(bool);
-  ~BoolFlag();
-  bool Read() const;
-  void Write(bool);
+  BoolFlag(bool f): flag_(f) {}
+  ~BoolFlag() {}
+  bool Read() const { return flag_;}
+  void Write(bool f) { flag_ = f; }
 
  private:
   DISALLOW_COPY_AND_ASSIGN(BoolFlag);
-  BoolFlag();
+  BoolFlag(): flag_(false) {}
   std::atomic<bool> flag_;
 };
-
