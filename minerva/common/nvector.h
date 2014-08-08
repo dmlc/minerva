2e8fd216f5d16fe6d41ab797f686f5b297f27dfd
Yutian Li <kevinleeone@gmail.com>
2014-08-08 14:20:28 +0800

    [norm-expansion] Comment NVector

--- minerva/common/nvector.h
+++ minerva/common/nvector.h
@@ -7,51 +7,54 @@ namespace minerva {
 
 template<class T>
 class NVector {
-public:
-	NVector() { }
-	explicit NVector(const Scale& size) {
-		data_.resize(size.Prod());
-		range_ = ScaleRange::MakeRange(Scale::Origin(size.NumDims()), size);
-	}
-	explicit NVector(const ScaleRange& r): range_(r) {
-		data_.resize(range_.Area());
-	}
+ public:
+  // Constructor
+  NVector() {}
+  explicit NVector(const Scale& size) {
+    data_.resize(size.Prod());
+    range_ = ScaleRange::MakeRange(Scale::Origin(size.NumDims()), size);
+  }
+  explicit NVector(const ScaleRange& r): range_(r) {
+    data_.resize(range_.Area());
+  }
   NVector(const std::vector<T>& d, const ScaleRange& r): data_(d), range_(r) {}
   NVector(const std::vector<T>& d, const Scale& size): data_(d) {
     range_ = ScaleRange::MakeRangeFromOrigin(size);
   }
-	NVector(const NVector& other): data_(other.data_), range_(other.range_) {}
-	const T& operator[] (const Scale& idx) const {
-		return data_[range_.Flatten(idx)];
-	}
-	T& operator[] (const Scale& idx) {
-		return data_[range_.Flatten(idx)];
-	}
-  bool operator == (const NVector<T>& other) const {
+  NVector(const NVector& other): data_(other.data_), range_(other.range_) {}
+  // Operator
+  const T& operator[] (const Scale& idx) const {
+    return data_[range_.Flatten(idx)];
+  }
+  T& operator[] (const Scale& idx) {
+    return data_[range_.Flatten(idx)];
+  }
+  bool operator== (const NVector<T>& other) const {
     return data_ == other.data_ && range_ == other.range_;
   }
-	size_t Length() const {
-		return data_.size();
-	}
-	Scale Size() const {
-		return range_.Dim();
-	}
-	int Size(int dim) const {
-		return range_.Dim()[dim];
-	}
-  bool IsEmpty() const {
-    return range_.Area() == 0;
+  // Utility function
+  size_t Length() const {
+    return data_.size();
   }
-	void Resize(const Scale& size) {
-		data_.resize(size.Prod());
-		range_ = ScaleRange::MakeRange(Scale::Origin(size.NumDims()), size);
-	}
+  Scale Size() const {
+    return range_.Dim();
+  }
+  int Size(int dim) const {
+    return range_.Dim()[dim];
+  }
+  bool IsEmpty() const {
+  return range_.Area() == 0;
+  }
+  void Resize(const Scale& size) {
+    data_.resize(size.Prod());
+    range_ = ScaleRange::MakeRange(Scale::Origin(size.NumDims()), size);
+  }
   const std::vector<T>& ToVector() const { return data_; }
-
+  // Iterator and mapping
   template<class U, class Fn>
   NVector<U> Map(Fn fn) const {
     std::vector<U> newdata;
-    for(const T& d : data_) {
+    for (const T& d: data_) {
       newdata.push_back(fn(d));
     }
     return NVector<U>(newdata, range_);
@@ -60,22 +63,23 @@ public:
   static NVector<T> ZipMap(const NVector<T>& nv1, const NVector<T>& nv2, Fn fn) {
     assert(nv1.range_ == nv2.range_);
     std::vector<T> newdata;
-    for(size_t i = 0; i < nv1.data_.size(); ++i) {
+    for (size_t i = 0; i < nv1.data_.size(); ++i) {
       newdata.push_back(fn(nv1.data_[i], nv2.data_[i]));
     }
     return NVector<T>(newdata, nv1.range_);
   }
   template<class Fn>
   void Foreach(Fn fn) {
-    for(T& d: data_) {
+    for (T& d: data_) {
       fn(d);
     }
   }
   typename std::vector<T>::const_iterator begin() const { return data_.begin(); }
   typename std::vector<T>::const_iterator end() const { return data_.end(); }
+
 private:
-	std::vector<T> data_;
-	ScaleRange range_;
+  std::vector<T> data_;
+  ScaleRange range_;
 };
 
 } // end of namespace minerva
