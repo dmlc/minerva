be81eda26661ca2f18add0c5fa649b353fdd2594
Kevin Lee <kevinleeone@gmail.com>
2014-08-31 16:24:41 +0800

    [new-chunk-compute-impl] Remove Chunk operators

--- minerva/chunk/chunk.h
+++ minerva/chunk/chunk.h
@@ -8,40 +8,19 @@
 namespace minerva {
 
 class Chunk {
-  friend class ChunkElewise;
-
  public:
   Chunk();
   Chunk(PhysicalDataNode* node);
   Chunk(const Chunk& other);
   PhysicalDataNode* data_node() const { return data_node_; }
   Chunk& operator=(const Chunk& other);
-
-  friend Chunk operator + (Chunk, Chunk);
-  friend Chunk operator - (Chunk, Chunk);
-  friend Chunk operator / (Chunk, Chunk);
-  friend Chunk operator + (Chunk, float);
-  friend Chunk operator - (Chunk, float);
-  friend Chunk operator * (Chunk, float);
-  friend Chunk operator / (Chunk, float);
-  friend Chunk operator + (float, Chunk);
-  friend Chunk operator - (float, Chunk);
-  friend Chunk operator * (float, Chunk);
-  friend Chunk operator / (float, Chunk);
-  void operator += (Chunk );
-  void operator -= (Chunk );
-  void operator /= (Chunk );
-  void operator += (float );
-  void operator -= (float );
-  void operator *= (float );
-  void operator /= (float );
-  Chunk operator - ();
-  // shape
+  // Shape
   const Scale& Size() const { return data_node_->data_.size; }
   int Size(int dim) const { return data_node_->data_.size[dim]; }
 
-  static Scale ComputeOffset(NVector<Chunk> ); // return merged size
-  static Chunk Merge(NVector<Chunk> );
+  // TODO deprecated, no partition for now
+  static Scale ComputeOffset(NVector<Chunk>); // Return merged size
+  static Chunk Merge(NVector<Chunk>);
   NVector<Chunk> Split(const NVector<Scale>& partsizes);
   // DAG building operations
   static std::vector<Chunk> Compute(const std::vector<Chunk>& params,
@@ -49,14 +28,6 @@ class Chunk {
 
  private:
   PhysicalDataNode* data_node_; // Set up in constructor
-};
-
-class ChunkElewise {
- public:
-  static Chunk Exp(Chunk);
-  static Chunk Ln(Chunk);
-  static Chunk Sigmoid(Chunk);
-  static Chunk Mult(Chunk, Chunk);
 };
 
 } // end of namespace minerva
