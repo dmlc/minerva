ada60d0b58374c9ad990a43a349aa4f6ecc3b9a5
Yutian Li <kevinleeone@gmail.com>
2014-09-11 14:49:17 +0800

    [removing-ldag] Simplify NO_IMPL

--- minerva/op/impl/bundle.h
+++ minerva/op/impl/bundle.h
@@ -1,24 +1,15 @@
 #pragma once
-#include <iostream>
-#include <map>
 #include "basic.h"
 #include "impl.h"
+#include <glog/logging.h>
+#include <iostream>
+#include <map>
 
 namespace minerva {
 
-template<class A1, class A2>
-void NO_IMPL(A1&, A2&) {
-  std::cout << "No implementation" << std::endl;
-}
-
-template<class A1, class A2, class A3>
-void NO_IMPL(A1&, A2&, A3&) {
-  std::cout << "No implementation" << std::endl;
-}
-
-template<class A1, class A2, class A3, class A4>
-void NO_IMPL(A1&, A2&, A3&, const A4&) {
-  std::cout << "No implementation" << std::endl;
+template<typename... Args>
+void NO_IMPL(Args&&...) {
+  CHECK(false) << "no implementation";
 }
 
 INSTALL_COMPUTE_FN(ArithmeticClosure, basic::Arithmetic, NO_IMPL, NO_IMPL);
