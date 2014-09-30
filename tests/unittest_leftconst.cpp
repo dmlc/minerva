6874daf3dd7d8684161473bc08b6cde41e4e5cae
Yutian Li <kevinleeone@gmail.com>
2014-09-30 12:03:40 +0800

    [master] LeftConst is not a unittest

---
+++ tests/unittest_leftconst.cpp
@@ -0,0 +1,30 @@
+#include "unittest_main.h"
+#include <minerva.h>
+#include <gtest/gtest.h>
+#include <iostream>
+
+using namespace minerva;
+using namespace std;
+
+TEST(LeftConst, LeftConst) {
+  auto& ms = MinervaSystem::Instance();
+  ms.current_device_id_ = cpuDevice;
+  int m = 8;
+  int k = 100;
+  NArray a = NArray::Randn({m, k}, 0.0, 1.0);
+  ms.current_device_id_ = gpuDevice;
+  NArray b = 1 - a;
+  NArray b1 = 1 - b;
+  NArray b2 = 2 - b1;
+  NArray b3 = 3 - b2;
+  NArray b4 = 4 - b3;
+  NArray b5 = 5 - b4;
+  auto in = a.Get();
+  auto res = b.Get();
+  auto in_ptr = in.get();
+  auto res_ptr = res.get();
+  for (int i = 0; i < a.Size().Prod(); ++i) {
+    EXPECT_FLOAT_EQ(1 - in_ptr[i], res_ptr[i]);
+  }
+}
+
---
+++ tests/unittest_main.h
@@ -0,0 +1,7 @@
+#pragma once
+#include <cstdint>
+
+extern uint64_t cpuDevice;
+#ifdef HAS_CUDA
+extern uint64_t gpuDevice;
+#endif
