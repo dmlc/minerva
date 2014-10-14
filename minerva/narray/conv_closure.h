8a12b3af68205b2b77c79f972661480119d4c128
Yutian Li <kevinleeone@gmail.com>
2014-10-14 09:59:27 +0800

    [cudnn-interface] WIP, working on OP

---
+++ minerva/narray/conv_closure.h
@@ -0,0 +1,36 @@
+#pragma once
+
+namespace minerva {
+
+struct ConvInfo {
+  int pad_height;
+  int pad_width;
+  int stride_vertical;
+  int stride_horizontal;
+};
+
+struct PoolingInfo {
+  enum Algorithm {
+    kMax,
+    kAverage
+  };
+  Algorithm algorithm;
+  int height;
+  int width;
+  int stride_vertical;
+  int stride_horizontal;
+};
+
+enum class SoftmaxAlgorithm {
+  kInstance,
+  kChannel
+};
+
+enum class ActivationAlgorithm {
+  kSigmoid,
+  kRelu,
+  kTanh
+};
+
+}  // namespace minerva
+
