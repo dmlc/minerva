3a6d9a7e2efaff9cf45053ac8cd2d4fac95156a6
SerailHydra <serailhydra@gmail.com>
2014-10-14 09:51:01 +0800

    mnist working with scipy.io.loadmat

--- owl/owl.cpp
+++ owl/owl.cpp
@@ -3,6 +3,10 @@
 #include <boost/python.hpp>
 #include <boost/python/stl_iterator.hpp>
 #include <boost/python/implicit.hpp>
+#include <boost/python/args.hpp>
+
+#include <iostream>
+using namespace std;
 
 #include "minerva.h"
 
@@ -20,17 +24,21 @@ void Initialize(bp::list args) {
   m::MinervaSystem::Instance().Initialize(&argc, &argv);
 }
 
-std::string LogicalDag() {
-  return m::MinervaSystem::Instance().logical_dag().PrintDag();
+uint64_t CreateCpuDevice() {
+  return m::MinervaSystem::Instance().device_manager().CreateCpuDevice();
+}
+
+void SetDevice(uint64_t id) {
+  m::MinervaSystem::Instance().current_device_id_ = id;
 }
 
 m::NArray Softmax(m::NArray m) {
   m::NArray maxval = m.Max(0);
   // NArray centered = m - maxval.Tile({m.Size(0), 1});
-  m::NArray centered = m.NormArithmetic(maxval, m::SUB);
+  m::NArray centered = m.NormArithmetic(maxval, m::ArithmeticType::kSub);
   m::NArray class_normalizer = m::Elewise::Ln(m::Elewise::Exp(centered).Sum(0)) + maxval;
   // return Elewise::Exp(m - class_normalizer.Tile({m.Size(0), 1}));
-  return m::Elewise::Exp(m.NormArithmetic(class_normalizer, m::SUB));
+  return m::Elewise::Exp(m.NormArithmetic(class_normalizer, m::ArithmeticType::kSub));
 }
 
 m::Scale ToScale(const bp::list& l) {
@@ -46,25 +54,29 @@ bp::list ToPythonList(const m::Scale& s) {
   return l;
 }
 
-m::NArray ZerosWrapper(const bp::list& s, const bp::list& np) {
-  return m::NArray::Zeros(ToScale(s), ToScale(np));
+m::NArray ZerosWrapper(const bp::list& s) {
+  return m::NArray::Zeros(ToScale(s));
 }
 
-m::NArray OnesWrapper(const bp::list& s, const bp::list& np) {
-  return m::NArray::Ones(ToScale(s), ToScale(np));
+m::NArray OnesWrapper(const bp::list& s) {
+  return m::NArray::Ones(ToScale(s));
 }
 
-m::NArray MakeNArrayWrapper(const bp::list& s, bp::list& val, const bp::list& np) {
+m::NArray RandnWrapper(const bp::list& s, float mean, float var) {
+  return m::NArray::Randn(ToScale(s), mean, var);
+}
+
+m::NArray MakeNArrayWrapper(const bp::list& s, bp::list& val) {
   size_t length = bp::len(val);
   std::shared_ptr<float> valptr(new float[length]);
   for(size_t i = 0; i < length; ++i) {
-    valptr.get()[i] = bp::extract<float>(val[i]);
+    valptr.get()[i] = bp::extract<float>(val[i] * 1.0);
   }
-  return m::NArray::MakeNArray(ToScale(s), valptr, ToScale(np));
+  return m::NArray::MakeNArray(ToScale(s), valptr);
 }
-
-m::NArray LoadFromFileWrapper(const bp::list& s, const std::string& fname, m::IFileLoader* loader, const bp::list& np) {
-  return m::NArray::LoadFromFile(ToScale(s), fname, loader, ToScale(np));
+/*
+m::NArray LoadFromFileWrapper(const bp::list& s, const std::string& fname, m::IFileLoader* loader) {
+  return m::NArray::LoadFromFile(ToScale(s), fname, loader);
 }
 
 class OneFileMBLoaderWrapper : public m::OneFileMBLoader {
@@ -72,13 +84,12 @@ class OneFileMBLoaderWrapper : public m::OneFileMBLoader {
   OneFileMBLoaderWrapper(const std::string& fname, const bp::list& shape):
     OneFileMBLoader(fname, ToScale(shape)) {}
 };
-
+*/
 bp::list NArrayToList(m::NArray narr) {
   bp::list l;
-  float* v = narr.Get();
+  std::shared_ptr<float> v = narr.Get();
   for(int i = 0; i < narr.Size().Prod(); ++i)
-    l.append(v[i]);
-  delete [] v;
+    l.append(v.get()[i]);
   return l;
 }
 
@@ -92,16 +103,20 @@ void WaitForEvalFinish() {
 BOOST_PYTHON_MODULE(libowl) {
   using namespace boost::python;
 
-  m::NArray (m::NArray::*fp_sum1)(int ) = &m::NArray::Sum;
-  m::NArray (m::NArray::*fp_max1)(int ) = &m::NArray::Max;
-  m::NArray (m::NArray::*fp_maxidx)(int ) = &m::NArray::MaxIndex;
-
   enum_<m::ArithmeticType>("arithmetic")
-    .value("add", m::ADD)
-    .value("sub", m::SUB)
-    .value("mul", m::MULT)
-    .value("div", m::DIV)
+    .value("add", m::ArithmeticType::kAdd)
+    .value("sub", m::ArithmeticType::kSub)
+    .value("mul", m::ArithmeticType::kMult)
+    .value("div", m::ArithmeticType::kDiv)
   ;
+
+  float (m::NArray::*sum0)() const = &m::NArray::Sum;
+  m::NArray (m::NArray::*sum1)(int) const = &m::NArray::Sum;
+  m::NArray (m::NArray::*sum2)(const m::Scale&) const = &m::NArray::Sum;
+
+  float (m::NArray::*max0)() const = &m::NArray::Max;
+  m::NArray (m::NArray::*max1)(int) const = &m::NArray::Max;
+  m::NArray (m::NArray::*max2)(const m::Scale&) const = &m::NArray::Max;
 
   //class_<m::Scale>("Scale");
   class_<m::NArray>("NArray")
@@ -120,12 +135,16 @@ BOOST_PYTHON_MODULE(libowl) {
     // matrix multiply
     .def(self * self)
     // reduction
-    .def("sum", fp_sum1)
-    .def("max", fp_max1)
-    .def("argmax", fp_maxidx)
+    .def("sum", sum0)
+    .def("sum", sum1)
+    .def("sum", sum2)
+    .def("max", max0)
+    .def("max", max1)
+    .def("max", max2)
+    .def("max_index", &m::NArray::MaxIndex)
     .def("count_zero", &m::NArray::CountZero)
     // normalize
-    .def("normalize", &m::NArray::NormArithmetic)
+    .def("norm_arithmetic", &m::NArray::NormArithmetic)
     // misc
     .def("trans", &m::NArray::Trans)
     .def("tofile", &m::NArray::ToFile)
@@ -133,32 +152,25 @@ BOOST_PYTHON_MODULE(libowl) {
     .def("eval", &m::NArray::Eval)
     .def("eval_async", &m::NArray::EvalAsync)
   ;
-
+/*
   // file loader
   class_<m::IFileLoader>("IFileLoader");
   class_<m::SimpleFileLoader, bases<m::IFileLoader>>("SimpleFileLoader");
   class_<m::FileFormat>("FileFormat")
     .def_readwrite("binary", &m::FileFormat::binary)
   ;
-  def("load_from_file", &owl::LoadFromFileWrapper);
-
-  // mb loader
-  class_<owl::OneFileMBLoaderWrapper>("MBLoader", init<std::string, bp::list>())
-    .def("load_next", &owl::OneFileMBLoaderWrapper::LoadNext)
-  ;
-
+*/
   // creators
   def("zeros", &owl::ZerosWrapper);
   def("ones", &owl::OnesWrapper);
   def("make_narray", &owl::MakeNArrayWrapper);
-  //def("random_randn", &m::NArray::Randn);
-  //def("zeros", &m::NArray::Zeros);
-  //def("ones", &m::NArray::Ones);
+  def("randn", &owl::RandnWrapper);
 
   // system
   //def("to_list", &owl::NArrayToList);
   def("initialize", &owl::Initialize);
-  def("logical_dag", &owl::LogicalDag);
+  def("create_cpu_device", &owl::CreateCpuDevice);
+  def("set_device", &owl::SetDevice);
   def("wait_eval", &owl::WaitForEvalFinish);
 
   // elewise
