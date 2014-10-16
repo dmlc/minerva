e403c6d7f556809d347dd5cc476bf52b1b770281
Kevin Lee <kevinleeone@gmail.com>
2014-10-16 19:06:04 +0800

    [master] Unittest for reduction

--- tests/unittest_reduction.cpp
+++ tests/unittest_reduction.cpp
@@ -7,56 +7,69 @@ using namespace std;
 
 TEST(Reduction, MaxOnFirstDimension) {
   Scale size{5, 3};
-  shared_ptr<float> data( new float[size.Prod()] );
+  shared_ptr<float> data(new float[size.Prod()], [](float* ptr) {
+    delete[] ptr;
+  });
   for (int i = 0; i < size.Prod(); ++i) {
     data.get()[i] = i;
   }
-  NArray na = NArray::MakeNArray(size, data, {1, 1});
+  NArray na = NArray::MakeNArray(size, data);
   NArray na2 = na.Max(0);
-  float* res = na2.Get();
+  auto res = na2.Get();
+  auto res_ptr = res.get();
   for (int i = 0; i < na2.Size().Prod(); ++i) {
-    EXPECT_FLOAT_EQ(res[i], 5 * i + 4);
+    EXPECT_FLOAT_EQ(res_ptr[i], 5 * i + 4);
   }
 }
 
 TEST(Reduction, MaxOnSecondDimension) {
   Scale size{5, 3};
-  shared_ptr<float> data(new float[size.Prod()]);
+  shared_ptr<float> data(new float[size.Prod()], [](float* ptr) {
+    delete[] ptr;
+  });
   for (int i = 0; i < size.Prod(); ++i) {
     data.get()[i] = i;
   }
-  NArray na = NArray::MakeNArray(size, data, {1, 1});
+  NArray na = NArray::MakeNArray(size, data);
   NArray na2 = na.Max(1);
-  float* res = na2.Get();
+  auto res = na2.Get();
+  auto res_ptr = res.get();
   for (int i = 0; i < na2.Size().Prod(); ++i) {
-    EXPECT_FLOAT_EQ(res[i], 10 + i);
+    EXPECT_FLOAT_EQ(res_ptr[i], 10 + i);
   }
 }
 
 TEST(Reduction, SumOnFirstDimension) {
   Scale size{5, 3};
-  shared_ptr<float> data(new float[size.Prod()]);
+  shared_ptr<float> data(new float[size.Prod()], [](float* ptr) {
+    delete[] ptr;
+  });
   for (int i = 0; i < size.Prod(); ++i) {
     data.get()[i] = i;
   }
-  NArray na = NArray::MakeNArray(size, data, {1, 1});
+  NArray na = NArray::MakeNArray(size, data);
   NArray na2 = na.Sum(0);
-  float* res = na2.Get();
+  auto res = na2.Get();
+  auto res_ptr = res.get();
   for (int i = 0; i < na2.Size().Prod(); ++i) {
-    EXPECT_FLOAT_EQ(res[i], 25 * i + 10);
+    EXPECT_FLOAT_EQ(res_ptr[i], 25 * i + 10);
   }
 }
 
 TEST(Reduction, SumOnSecondDimension) {
   Scale size{5, 3};
-  shared_ptr<float> data(new float[size.Prod()]);
+  shared_ptr<float> data(new float[size.Prod()], [](float* ptr) {
+    delete[] ptr;
+  });
   for (int i = 0; i < size.Prod(); ++i) {
     data.get()[i] = i;
   }
-  NArray na = NArray::MakeNArray(size, data, {1, 1});
+  NArray na = NArray::MakeNArray(size, data);
   NArray na2 = na.Sum(1);
-  float* res = na2.Get();
+  auto res = na2.Get();
+  auto res_ptr = res.get();
   for (int i = 0; i < na2.Size().Prod(); ++i) {
-    EXPECT_FLOAT_EQ(res[i], 3 * i + 15);
+    EXPECT_FLOAT_EQ(res_ptr[i], 3 * i + 15);
   }
 }
+
