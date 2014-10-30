4f3b3639906a40127f9ae5dfc7c9014507ed6384
Kevin Lee <kevinleeone@gmail.com>
2014-10-30 17:04:56 +0800

    [mem-debug] Sub for multi dimension

--- minerva/op/impl/cuda/cuda_perform.h
+++ minerva/op/impl/cuda/cuda_perform.h
@@ -11,10 +11,10 @@ namespace cuda {
 
 void CudaPerformDotMult(float*, float*, float*, size_t, cudaStream_t);
 void CudaPerformDotDiv(float*, float*, float*, size_t, cudaStream_t);
-void CudaPerformAdd(float* a, float* b, float* c, int m, int n, cublasHandle_t);
-void CudaPerformSub(float* a, float* b, float* c, int m, int n, cublasHandle_t);
+void CudaPerformAdd(float* a, float* b, float* c, size_t, cublasHandle_t);
+void CudaPerformSub(float* a, float* b, float* c, size_t, cublasHandle_t);
 void CudaPerformMatMult(float*, float*, float*, int, int, int, cublasHandle_t);
-void CudaPerformScale(float* in_data, float* res_data, int m, int n, float val, cublasHandle_t);
+void CudaPerformScale(float* in_data, float* res_data, size_t, float val, cublasHandle_t);
 void CudaPerformTranspose(float* a, float* c, int m, int n, cublasHandle_t);
 
 void CudaPerformConstAdd(float* in, float* out, float val, size_t, cudaStream_t);
