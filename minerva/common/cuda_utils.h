5c38e10a674ec8cd3034693e0ba5e6a0a2739787
Kevin Lee <kevinleeone@gmail.com>
2014-11-04 21:20:55 +0800

    [master] Add curand error types

--- minerva/common/cuda_utils.h
+++ minerva/common/cuda_utils.h
@@ -4,11 +4,12 @@
 #include <cuda_runtime.h>
 #include <cublas_v2.h>
 #include <cudnn.h>
+#include <curand.h>
 #include <glog/logging.h>
 #include <string>
 #include <algorithm>
 
-inline const char* CublasGetErrorEnum(cublasStatus_t error) {
+inline const char* CublasGetErrorString(cublasStatus_t error) {
   switch (error) {
     case CUBLAS_STATUS_SUCCESS:
       return "CUBLAS_STATUS_SUCCESS";
@@ -62,6 +63,38 @@ inline const char* CudnnGetErrorString(cudnnStatus_t status) {
   return "Unknown cuDNN status";
 }
 
+inline const char* CurandGetErrorString(curandStatus_t status) {
+  switch (status) {
+    case CURAND_STATUS_SUCCESS:
+      return "CURAND_STATUS_SUCCESS";
+    case CURAND_STATUS_VERSION_MISMATCH:
+      return "CURAND_STATUS_VERSION_MISMATCH";
+    case CURAND_STATUS_NOT_INITIALIZED:
+      return "CURAND_STATUS_NOT_INITIALIZED";
+    case CURAND_STATUS_ALLOCATION_FAILED:
+      return "CURAND_STATUS_ALLOCATION_FAILED";
+    case CURAND_STATUS_TYPE_ERROR:
+      return "CURAND_STATUS_TYPE_ERROR";
+    case CURAND_STATUS_OUT_OF_RANGE:
+      return "CURAND_STATUS_OUT_OF_RANGE";
+    case CURAND_STATUS_LENGTH_NOT_MULTIPLE:
+      return "CURAND_STATUS_LENGTH_NOT_MULTIPLE";
+    case CURAND_STATUS_DOUBLE_PRECISION_REQUIRED:
+      return "CURAND_STATUS_DOUBLE_PRECISION_REQUIRED";
+    case CURAND_STATUS_LAUNCH_FAILURE:
+      return "CURAND_STATUS_LAUNCH_FAILURE";
+    case CURAND_STATUS_PREEXISTING_FAILURE:
+      return "CURAND_STATUS_PREEXISTING_FAILURE";
+    case CURAND_STATUS_INITIALIZATION_FAILED:
+      return "CURAND_STATUS_INITIALIZATION_FAILED";
+    case CURAND_STATUS_ARCH_MISMATCH:
+      return "CURAND_STATUS_ARCH_MISMATCH";
+    case CURAND_STATUS_INTERNAL_ERROR:
+      return "CURAND_STATUS_INTERNAL_ERROR";
+  }
+  return "Unknown cuRAND status";
+}
+
 #define CheckCudaError(msg) { \
   cudaError_t e = cudaGetLastError(); \
   CHECK_EQ(e, cudaSuccess) << msg << " CUDA: " << cudaGetErrorString(e); \
@@ -74,12 +107,17 @@ inline const char* CudnnGetErrorString(cudnnStatus_t status) {
 
 #define CUBLAS_CALL(func) { \
   cublasStatus_t e = (func); \
-  CHECK_EQ(e, CUBLAS_STATUS_SUCCESS) << "cuBLAS: " << CublasGetErrorEnum(e); \
+  CHECK_EQ(e, CUBLAS_STATUS_SUCCESS) << "cuBLAS: " << CublasGetErrorString(e); \
 }
 
 #define CUDNN_CALL(func) { \
   cudnnStatus_t e = (func); \
   CHECK_EQ(e, CUDNN_STATUS_SUCCESS) << "cuDNN: " << CudnnGetErrorString(e); \
+}
+
+#define CURAND_CALL(func) { \
+  curandStatus_t e = (func); \
+  CHECK_EQ(e, CURAND_STATUS_SUCCESS) << "cuRAND: " << CudnnGetErrorString(e); \
 }
 
 #endif
