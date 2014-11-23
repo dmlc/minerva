e02b1985b1ca6cd0f5101a77b545936f14616534
Minjie Wang <wmjlyjemaine@gmail.com>
2014-11-23 00:22:10 -0500

    [app_layer] Add explanation for numpy array conversion

--- owl/owl/__init__.py
+++ owl/owl/__init__.py
@@ -12,6 +12,9 @@ ones = _owl.ones
 randn = _owl.randn
 randb = _owl.randb
 make_narray = _owl.make_narray
+
+# Convert numpy array into minerva array. ATTENTION: this will lead to
+# a transpose due to the different storage priority
 def from_nparray(nparr):
     return _owl.make_narray(list(nparr.shape[::-1]), nparr.flatten().tolist())
 
