c6a8a36ac8da7ac8e48726d14d7e4350a2c509a2
Minjie Wang <wmjlyjemaine@gmail.com>
2014-12-20 13:49:47 -0500

    [show-minjie] modified demo for nips14

--- owl/demo/demo2.py
+++ owl/demo/demo2.py
@@ -6,7 +6,7 @@ import demo_common
 x = owl.randn([227, 227, 3, 256], 0.0, 1)
 w = owl.randn([11, 11, 3, 96], 0.0, 0.1)
 b = owl.zeros([96])
-conv = co.Convolver(0, 0, 4, 4) #pad_h, pad_w, stride_h, stride_w
+conv = co.Convolver(pad_h=0, pad_w=0, stride_v=4, stride_h=4)
 
 y = conv.ff(x, w, b)
 print y.tonparray()
--- owl/demo/demo3.py
+++ owl/demo/demo3.py
@@ -7,9 +7,7 @@ x2 = owl.randn([784, 128], 0.0, 0.1)
 w = owl.randn([512, 784], 0.0, 0.1)
 b = owl.zeros([512, 1])
 
-owl.set_device(dc.gpu0)
 y1 = w * x1 + b
-owl.set_device(dc.gpu1)
 y2 = w * x2 + b
 gw = y1 * x1.trans() + y2 * x2.trans()
 print gw.tonparray()
