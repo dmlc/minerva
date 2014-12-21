8301a7e1c5a2c360e7961e9059070a45e92813b6
Zhang Xianyi <traits.zhang@gmail.com>
2014-12-22 02:32:16 +0800

    Split the unittest to Gpu and Cpu.

--- tests/unittest_activation.cpp
+++ tests/unittest_activation.cpp
@@ -3,16 +3,14 @@
 
 using namespace std;
 using namespace minerva;
-
-TEST(Activation, SigmoidForward) {
+#ifdef HAS_CUDA
+TEST(Activation, GpuSigmoidForward) {
   auto& ms = MinervaSystem::Instance();
   Scale input_size{7, 6, 3, 2};
 
   ms.current_device_id_ = cpu_device;
   ImageBatch input = NArray::Randn(input_size, 0, 1);
-#ifdef HAS_CUDA
   ms.current_device_id_ = gpu_device;
-#endif
   ImageBatch output = Convolution::ActivationForward(input, ActivationAlgorithm::kSigmoid);
   auto input_ptr = input.Get();
   auto output_ptr = output.Get();
@@ -21,15 +19,13 @@ TEST(Activation, SigmoidForward) {
   }
 }
 
-TEST(Activation, ReluForward) {
+TEST(Activation, GpuReluForward) {
   auto& ms = MinervaSystem::Instance();
   Scale input_size{7, 6, 3, 2};
 
   ms.current_device_id_ = cpu_device;
   ImageBatch input = NArray::Randn(input_size, 0, 1);
-#ifdef HAS_CUDA
   ms.current_device_id_ = gpu_device;
-#endif
   ImageBatch output = Convolution::ActivationForward(input, ActivationAlgorithm::kRelu);
   auto input_ptr = input.Get();
   auto output_ptr = output.Get();
@@ -38,15 +34,56 @@ TEST(Activation, ReluForward) {
   }
 }
 
-TEST(Activation, TanhForward) {
+TEST(Activation, GpuTanhForward) {
   auto& ms = MinervaSystem::Instance();
   Scale input_size{7, 6, 3, 2};
 
   ms.current_device_id_ = cpu_device;
   ImageBatch input = NArray::Randn(input_size, 0, 1);
-#ifdef HAS_CUDA
   ms.current_device_id_ = gpu_device;
+  ImageBatch output = Convolution::ActivationForward(input, ActivationAlgorithm::kTanh);
+  auto input_ptr = input.Get();
+  auto output_ptr = output.Get();
+  for (int i = 0; i < input_size.Prod(); ++i) {
+    EXPECT_FLOAT_EQ(output_ptr.get()[i], tanh(input_ptr.get()[i]));
+  }
+}
 #endif
+
+TEST(Activation, CpuSigmoidForward) {
+  auto& ms = MinervaSystem::Instance();
+  Scale input_size{7, 6, 3, 2};
+
+  ms.current_device_id_ = cpu_device;
+  ImageBatch input = NArray::Randn(input_size, 0, 1);
+  ImageBatch output = Convolution::ActivationForward(input, ActivationAlgorithm::kSigmoid);
+  auto input_ptr = input.Get();
+  auto output_ptr = output.Get();
+  for (int i = 0; i < input_size.Prod(); ++i) {
+    EXPECT_FLOAT_EQ(output_ptr.get()[i], 1 / (1 + exp(-input_ptr.get()[i])));
+  }
+}
+
+TEST(Activation, CpuReluForward) {
+  auto& ms = MinervaSystem::Instance();
+  Scale input_size{7, 6, 3, 2};
+
+  ms.current_device_id_ = cpu_device;
+  ImageBatch input = NArray::Randn(input_size, 0, 1);
+  ImageBatch output = Convolution::ActivationForward(input, ActivationAlgorithm::kRelu);
+  auto input_ptr = input.Get();
+  auto output_ptr = output.Get();
+  for (int i = 0; i < input_size.Prod(); ++i) {
+    EXPECT_FLOAT_EQ(output_ptr.get()[i], 0 < input_ptr.get()[i] ? input_ptr.get()[i] : 0);
+  }
+}
+
+TEST(Activation, CpuTanhForward) {
+  auto& ms = MinervaSystem::Instance();
+  Scale input_size{7, 6, 3, 2};
+
+  ms.current_device_id_ = cpu_device;
+  ImageBatch input = NArray::Randn(input_size, 0, 1);
   ImageBatch output = Convolution::ActivationForward(input, ActivationAlgorithm::kTanh);
   auto input_ptr = input.Get();
   auto output_ptr = output.Get();
