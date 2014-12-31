c0c7bbbcc0fc5760c95ebb26d123656eba5e5372
Minjie Wang <wmjlyjemaine@gmail.com>
2014-12-31 17:11:38 -0500

    [doc] add doxygen stubs

---
+++ html/search/all_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['minerva_3a_20a_20fast_20and_20flexible_20system_20for_20deep_20learning',['Minerva: a fast and flexible system for deep learning',['../md_README.html',1,'']]]
+];
