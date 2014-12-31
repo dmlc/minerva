0a0ecd12af8084432563b192ed2dec67d1489232
Minjie Wang <wmjlyjemaine@gmail.com>
2014-12-31 00:19:52 -0500

    [develop] add back mnist apps

---
+++ owl/apps/mnist/mnist_cnn.py
@@ -0,0 +1,123 @@
+import sys,os
+import time
+import math
+import numpy as np
+import subprocess
+
+import imageio
+import owl
+import owl.elewise as ele
+import owl.conv as conv
+
+class MNISTCNNModel:
+    def __init__(self):
+        self.weights = []
+        self.bias = []
+        self.weightdelta = []
+        self.biasdelta = []
+        self.convs = [
+            conv.Convolver(0, 0, 1, 1),
+            conv.Convolver(2, 2, 1, 1),
+        ];
+        self.poolings = [
+            conv.Pooler(2, 2, 2, 2, conv.pool_op.max),
+            conv.Pooler(3, 3, 3, 3, conv.pool_op.max)
+        ];
+    def init_random(self):
+        self.weights = [
+            owl.randn([5, 5, 1, 16], 0.0, 0.1),
+            owl.randn([5, 5, 16, 32], 0.0, 0.1),
+            owl.randn([10, 512], 0.0, 0.1)
+        ];
+        self.weightdelta = [
+            owl.zeros([5, 5, 1, 16]),
+            owl.zeros([5, 5, 16, 32]),
+            owl.zeros([10, 512])
+        ];
+        self.bias = [
+            owl.zeros([16]),
+            owl.zeros([32]),
+            owl.zeros([10, 1])
+        ];
+        self.biasdelta = [
+            owl.zeros([16]),
+            owl.zeros([32]),
+            owl.zeros([10, 1])
+        ];
+
+
+def print_training_accuracy(o, t, mbsize, prefix):
+    predict = o.reshape([10, mbsize]).max_index(0)
+    ground_truth = t.reshape([10, mbsize]).max_index(0)
+    correct = (predict - ground_truth).count_zero()
+    print prefix, 'error: {}'.format((mbsize - correct) * 1.0 / mbsize)
+
+def bpprop(model, samples, label):
+    num_layers = 9
+    num_samples = samples.shape[-1]
+    fc_shape = [512, num_samples]
+
+    acts = [None] * num_layers
+    errs = [None] * num_layers
+    weightgrad = [None] * len(model.weights)
+    biasgrad = [None] * len(model.bias)
+
+    acts[0] = samples
+    acts[1] = ele.relu(model.convs[0].ff(acts[0], model.weights[0], model.bias[0]))
+    acts[2] = model.poolings[0].ff(acts[1])
+    acts[3] = ele.relu(model.convs[1].ff(acts[2], model.weights[1], model.bias[1]))
+    acts[4] = model.poolings[1].ff(acts[3])
+    acts[5] = model.weights[2] * acts[4].reshape(fc_shape) + model.bias[2]
+
+    out = conv.softmax(acts[5], conv.soft_op.instance)
+
+    errs[5] = out - label
+    errs[4] = (model.weights[2].trans() * errs[5]).reshape(acts[4].shape)
+    errs[3] = ele.relu_back(model.poolings[1].bp(errs[4], acts[4], acts[3]), acts[3])
+    errs[2] = model.convs[1].bp(errs[3], model.weights[1])
+    errs[1] = ele.relu_back(model.poolings[0].bp(errs[2], acts[2], acts[1]), acts[1])
+
+    weightgrad[2] = errs[5] * acts[4].reshape(fc_shape).trans()
+    biasgrad[2] = errs[5].sum(1)
+    weightgrad[1] = model.convs[1].weight_grad(errs[3], acts[2])
+    biasgrad[1] = model.convs[1].bias_grad(errs[3])
+    weightgrad[0] = model.convs[0].weight_grad(errs[1], acts[0])
+    biasgrad[0] = model.convs[0].bias_grad(errs[1])
+    return (out, weightgrad, biasgrad)
+
+def train_network(model, num_epochs = 100, minibatch_size = 256, lr = 0.01, mom = 0.75, wd = 5e-4):
+    owl.set_device(owl.create_gpu_device(0))
+    count = 0
+    # load data
+    (train_data, test_data) = imageio.load_mb_from_mat("mnist_all.mat", minibatch_size)
+    num_test_samples = test_data[0].shape[0]
+    test_samples = owl.from_nparray(test_data[0]).reshape([28, 28, 1, num_test_samples])
+    test_labels = owl.from_nparray(test_data[1])
+    for i in xrange(num_epochs):
+        print "---Epoch #", i
+        last = time.time()
+        for (mb_samples, mb_labels) in train_data:
+            num_samples = mb_samples.shape[0]
+            data = owl.from_nparray(mb_samples).reshape([28, 28, 1, num_samples])
+            label = owl.from_nparray(mb_labels)
+            out, weightgrad, biasgrad = bpprop(model, data, label)
+            for k in range(3):
+                model.weightdelta[k] = mom * model.weightdelta[k] - lr / num_samples * weightgrad[k] - lr * wd * model.weights[k]
+                model.biasdelta[k] = mom * model.biasdelta[k] - lr / num_samples * biasgrad[k]
+                model.weights[k] += model.weightdelta[k]
+                model.bias[k] += model.biasdelta[k]
+            
+            count = count + 1
+            if (count % 20) == 0:
+                print_training_accuracy(out, label, num_samples, 'Training')
+        print '----End of Epoch #', i, 'time:', time.time() - last
+        # do test
+        out, _, _  = bpprop(model, test_samples, test_labels)
+        print_training_accuracy(out, test_labels, num_test_samples, 'Testing')
+
+if __name__ == '__main__':
+    owl.initialize(sys.argv)
+    owl.create_cpu_device()
+    model = MNISTCNNModel()
+    model.init_random()
+    train_network(model)
---
+++ owl/apps/mnist/mnist_mlp.py
@@ -0,0 +1,83 @@
+import sys,os
+import math
+import owl
+import owl.elewise as ele
+import owl.conv as co
+import numpy as np
+import imageio
+
+class MnistTrainer:
+    def __init__(self, data_file='mnist_all.mat', num_epochs=100, mb_size=256, eps_w=0.01, eps_b=0.01):
+        self.cpu = owl.create_cpu_device()
+        self.gpu = owl.create_gpu_device(0)
+        self.data_file = data_file
+        self.num_epochs=num_epochs
+        self.mb_size=mb_size
+        self.eps_w=eps_w
+        self.eps_b=eps_b
+        # init weight
+        l1 = 784; l2 = 256; l3 = 10
+        self.l1 = l1; self.l2 = l2; self.l3 = l3
+        self.w1 = owl.randn([l2, l1], 0.0, math.sqrt(4.0 / (l1 + l2)))
+        self.w2 = owl.randn([l3, l2], 0.0, math.sqrt(4.0 / (l2 + l3)))
+        self.b1 = owl.zeros([l2, 1])
+        self.b2 = owl.zeros([l3, 1])
+
+    def run(self):
+        (train_data, test_data) = imageio.load_mb_from_mat(self.data_file, self.mb_size)
+        np.set_printoptions(linewidth=200)
+        num_test_samples = test_data[0].shape[0]
+        (test_samples, test_labels) = map(lambda npdata : owl.from_nparray(npdata), test_data)
+        count = 1
+        for epoch in range(self.num_epochs):
+            print '---Start epoch #%d' % epoch
+            # train
+            for (mb_samples, mb_labels) in train_data:
+                num_samples = mb_samples.shape[0]
+
+                owl.set_device(self.cpu)
+                a1 = owl.from_nparray(mb_samples)
+                target = owl.from_nparray(mb_labels)
+                owl.set_device(self.gpu)
+
+                # ff
+                a2 = ele.sigm(self.w1 * a1 + self.b1)
+                a3 = ele.sigm(self.w2 * a2 + self.b2)
+                # softmax & error
+                out = co.softmax(a3)
+                s3 = out - target
+                # bp
+                s3 = ele.mult(s3, 1 - s3)
+                s2 = self.w2.trans() * s3
+                s2 = ele.mult(s2, 1 - s2)
+                # grad
+                gw1 = s2 * a1.trans() / num_samples
+                gb1 = s2.sum(1) / num_samples
+                gw2 = s3 * a2.trans() / num_samples
+                gb2 = s3.sum(1) / num_samples
+                # update
+                self.w1 -= self.eps_w * gw1
+                self.w2 -= self.eps_w * gw2
+                self.b1 -= self.eps_b * gb1
+                self.b2 -= self.eps_b * gb2
+
+                if (count % 40 == 0):
+                    correct = out.max_index(0) - target.max_index(0)
+                    val = correct.tolist()
+                    print 'Training error:', (float(num_samples) - val.count(0.0)) / num_samples
+                    # test
+                    a1 = test_samples
+                    a2 = ele.sigm(self.w1 * a1 + self.b1)
+                    a3 = ele.sigm(self.w2 * a2 + self.b2)
+                    correct = a3.max_index(0) - test_labels.max_index(0)
+                    val = correct.tolist()
+                    #print val
+                    print 'Testing error:', (float(num_test_samples) - val.count(0.0)) / num_test_samples
+                count = count + 1
+
+            print '---Finish epoch #%d' % epoch
+
+if __name__ == '__main__':
+    owl.initialize(sys.argv)
+    trainer = MnistTrainer(num_epochs = 10)
+    trainer.run()
