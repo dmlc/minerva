e4a5fd05e1a761ca22f921276ad1bc3c658da903
Minjie Wang <wmjlyjemaine@gmail.com>
2015-01-04 11:44:45 -0500

    [doc] remove htmls

--- html/search/pages_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['minerva_3a_20a_20fast_20and_20flexible_20system_20for_20deep_20learning',['Minerva: a fast and flexible system for deep learning',['../md_README.html',1,'']]]
-];
