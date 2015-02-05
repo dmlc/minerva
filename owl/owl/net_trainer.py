06b340e4d92f3e3ebc0a7a5f026bfd985774d19f
tianjun <xiaotj1990327@gmail.com>
2015-02-05 13:19:31 +0800

    test VGG google, add sh

--- owl/owl/net_trainer.py
+++ owl/owl/net_trainer.py
@@ -15,28 +15,27 @@ if __name__ == "__main__":
     builder = CaffeNetBuilder(sys.argv[1], sys.argv[2])
     owl_net = net.Net()
     builder.build_net(owl_net)
-    builder.init_net_from_file(owl_net, '/home/tianjun/releaseversion/minerva/owl/apps/imagenet_googlenet/Googmodel/epoch0/')
-    #builder.init_net_from_file(owl_net, '/home/tianjun/releaseversion/minerva/owl/apps/imagenet_googlenet/VGGmodel/epoch0/')
-    #builder.init_net_from_file(owl_net, '/home/tianjun/releaseversion/minerva/owl/apps/imagenet_googlenet/Alexmodel/epoch0/')
+    builder.init_net_from_file(owl_net, sys.argv[3])
+    accunitname = sys.argv[4]
+    last = time.time()
     
     for iteridx in range(owl_net.solver.max_iter):
         owl_net.forward('TRAIN')
         owl_net.backward('TRAIN')
         owl_net.weight_update()
         
-        last = time.time()
-        owl_net.get_units_by_name('loss3/loss3')[0].ff_y.wait_for_eval()
+        #print owl_net.get_units_by_name(accunitname)[0].acc
+        owl_net.get_units_by_name(accunitname)[0].ff_y.wait_for_eval()
         print "Finished training 1 minibatch"
         print "time: %s" % (time.time() - last)
+        last = time.time()
         continue
 
         #decide whether to test
         if (iteridx + 1) % owl_net.solver.test_interval == 0:
             acc_num = 0
             test_num = 0
-            for testiteridx in range(owl_net.solver.max_iter):
+            for testiteridx in range(owl_net.solver.test_iter):
                 owl_net.forward('TEST')
                 accunit = owl_net.get_units_by_name('loss3/top-1')[0]
                 print "Accuracy this mb: %f" % (accunit.acc)
-                #acc_num += accunit.acc * accunit.minibatch_size
-                #test_num += accunit.minibatch_size
