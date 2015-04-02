6408d1740929e5188a60f2747a9a70ebc62d4302
chuntao hong <chuntao.hong@gmail.com>
2015-04-02 11:01:37 +0800

    ps running again

--- owl_environ.sh
+++ owl_environ.sh
@@ -1,6 +1,6 @@
 if [[ -z ${OWL_ENABLED} ]]; then
   export OWL_ENABLED=true
-  SCRIPTPATH="$(cd $(dirname $0); pwd -P)"
+  SCRIPTPATH="$(pwd -P)"
   export PYTHONPATH=${SCRIPTPATH}/release/owl:${SCRIPTPATH}/owl:$PYTHONPATH
   export PS1="(Owl Ready) ${PS1}"
 else
