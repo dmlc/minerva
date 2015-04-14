09ecfa1550cf95a17fb9aaa6688b1842ed9a486b
Yutian Li <hotpxless@gmail.com>
2015-04-14 11:35:03 +0800

    [athena-cython] Typo

--- minerva/common/concurrent_unordered_map.h
+++ minerva/common/concurrent_unordered_map.h
@@ -10,34 +10,34 @@ class ConcurrentUnorderedMap {
   DISALLOW_COPY_AND_MOVE(ConcurrentUnorderedMap);
   ~ConcurrentUnorderedMap() = default;
   V& operator[](const K& k) {
-    WriterLock lock(l_);
+    WriterLock lock(m_);
     return map_[k];
   }
   size_t Erase(const K& k) {
-    WriterLock lock(l_);
+    WriterLock lock(m_);
     return map_.erase(k);
   }
   size_t Insert(const typename std::unordered_map<K, V>::value_type& v) {
-    WriterLock lock(l_);
+    WriterLock lock(m_);
     return map_.insert(v).second;
   }
   V& At(const K& k) {
-    ReaderLock lock(l_);
+    ReaderLock lock(m_);
     return map_.at(k);
   }
   const V& At(const K& k) const {
-    ReaderLock lock(l_);
+    ReaderLock lock(m_);
     return map_.at(k);
   }
   size_t Size() const {
-    ReaderLock lock(l_);
+    ReaderLock lock(m_);
     return map_.size();
   }
   void LockRead() const {
-    l_.LockShared();
+    m_.LockShared();
   }
   void UnlockRead() const {
-    l_.UnlockShared();
+    m_.UnlockShared();
   }
   std::unordered_map<K, V>& VolatilePayload() {
     return map_;
