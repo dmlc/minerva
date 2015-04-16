b3e817389809493adadd27366e36b7245867f771
Yutian Li <hotpxless@gmail.com>
2015-04-16 22:49:20 +0800

    [athena-cython] my shared mutex is not slow at all!

--- minerva/common/concurrent_unordered_map.h
+++ minerva/common/concurrent_unordered_map.h
@@ -1,7 +1,5 @@
 #pragma once
 #include <unordered_map>
-#include <boost/thread/locks.hpp>
-#include <boost/thread/shared_mutex.hpp>
 #include "common/common.h"
 
 template<typename K, typename V> class ConcurrentUnorderedMap {
@@ -10,34 +8,34 @@ template<typename K, typename V> class ConcurrentUnorderedMap {
   DISALLOW_COPY_AND_ASSIGN(ConcurrentUnorderedMap);
   ~ConcurrentUnorderedMap() = default;
   V& operator[](const K& k) {
-    WriteLock lock(l_);
+    WriterLock lock(m_);
     return map_[k];
   }
   size_t Erase(const K& k) {
-    WriteLock lock(l_);
+    WriterLock lock(m_);
     return map_.erase(k);
   }
   size_t Insert(const typename std::unordered_map<K, V>::value_type& v) {
-    WriteLock lock(l_);
+    WriterLock lock(m_);
     return map_.insert(v).second;
   }
   V& At(const K& k) {
-    ReadLock lock(l_);
+    ReaderLock lock(m_);
     return map_.at(k);
   }
   const V& At(const K& k) const {
-    ReadLock lock(l_);
+    ReaderLock lock(m_);
     return map_.at(k);
   }
   size_t Size() const {
-    ReadLock lock(l_);
+    ReaderLock lock(m_);
     return map_.size();
   }
   void LockRead() const {
-    l_.lock_shared();
+    ReaderLock::Lock(m_);
   }
   void UnlockRead() const {
-    l_.unlock_shared();
+    ReaderLock::Unlock(m_);
   }
   std::unordered_map<K, V>& VolatilePayload() {
     return map_;
@@ -47,10 +45,10 @@ template<typename K, typename V> class ConcurrentUnorderedMap {
   }
 
  private:
-  typedef boost::shared_mutex Lock;
-  typedef boost::unique_lock<Lock> WriteLock;
-  typedef boost::shared_lock<Lock> ReadLock;
-  mutable Lock l_;
+  typedef minerva::common::SharedMutex Mutex;
+  typedef minerva::common::ReaderLock<Mutex> ReaderLock;
+  typedef minerva::common::WriterLock<Mutex> WriterLock;
+  mutable Mutex m_;
   std::unordered_map<K, V> map_;
 };
 
