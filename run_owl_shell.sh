a67ae949541c602588ca8e36d86187724fcebcb5
Minjie Wang <wmjlyjemaine@gmail.com>
2015-04-18 19:35:29 -0400

    [master] run_owl_shell could run without ipython

--- run_owl_shell.sh
+++ run_owl_shell.sh
@@ -1,3 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
-ipython -i owl/minerva_start.py
+PYTHON=
+
+if [ `command -v ipython >/dev/null 2>&1` ]; then
+  PYTHON=python
+else
+  PYTHON=ipython
+fi
+
+$PYTHON -i owl/minerva_start.py
