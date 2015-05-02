81e89c0aec1ebfa78a7a9f4e48781ef56d474a86
sneakerkg <xiaotj1990327@gmail.com>
2015-05-02 12:36:54 +0800

    add viusalization tool

--- owl/owl/net/trainer.py
+++ owl/owl/net/trainer.py
@@ -5,6 +5,8 @@ import numpy as np
 import owl
 from net import Net
 from net_helper import CaffeNetBuilder
+from caffe import *
+from PIL import Image
 
 class NetTrainer:
     ''' Class for DNN training.
@@ -201,3 +203,145 @@ class FeatureExtractor:
             print "Finish One Batch %d" % (batch_dir)
             batch_dir += 1
         feature_file.close()
+
+class FilterVisualizer:
+    ''' Class of filter visualizer.
+    Find the most interested patches of a filter to demostrate pattern that filter insterested in. 
+
+    :ivar str solver_file: name of the solver_file, it will tell Minerva the network configuration and model saving path 
+    :ivar snapshot: saved model snapshot index
+    :ivar str layer_name: name of the ayer that produce feature 
+    :ivar str result_path: path for the result of visualization
+    :ivar gpu: the gpu to run testing
+
+    '''
+    
+    
+    def __init__(self, solver_file, snapshot, layer_name, result_path, gpu_idx = 0):
+        self.solver_file = solver_file
+        self.snapshot = snapshot
+        self.layer_name = layer_name
+        self.result_path = result_path
+        self.gpu = owl.create_gpu_device(gpu_idx)
+        owl.set_device(self.gpu)
+
+    def build_net(self):
+        self.owl_net = Net()
+        self.builder = CaffeNetBuilder(self.solver_file)
+        self.snapshot_dir = self.builder.snapshot_dir
+        self.builder.build_net(self.owl_net)
+        self.owl_net.compute_size('TEST')
+        self.builder.init_net_from_file(self.owl_net, self.snapshot_dir, self.snapshot)
+
+    def run(s):
+        #Need Attention, here we may have multiple data layer, just choose the TEST layer
+        data_unit = None
+        for i in range(len(s.owl_net.name_to_uid['data'])):
+            if s.owl_net.units[s.owl_net.name_to_uid['data'][i]].params.include[0].phase == 1:
+                data_unit = s.owl_net.units[s.owl_net.name_to_uid['data'][i]]
+        assert(data_unit)
+       
+        bp = BlobProto()
+        #get mean file
+        if len(data_unit.params.transform_param.mean_file) == 0:
+            mean_data = np.ones([3, 256, 256], dtype=np.float32)
+            assert(len(data_unit.params.transform_param.mean_value) == 3)
+            mean_data[0] = data_unit.params.transform_param.mean_value[0]
+            mean_data[1] = data_unit.params.transform_param.mean_value[1]
+            mean_data[2] = data_unit.params.transform_param.mean_value[2]
+            h_w = 256
+        else:    
+            with open(data_unit.params.transform_param.mean_file, 'rb') as f:
+                bp.ParseFromString(f.read())
+            mean_narray = np.array(bp.data, dtype=np.float32)
+            h_w = np.sqrt(np.shape(mean_narray)[0] / 3)
+            mean_data = np.array(bp.data, dtype=np.float32).reshape([3, h_w, h_w])
+        #get the cropped img
+        crop_size = data_unit.params.transform_param.crop_size
+        crop_h_w = (h_w - crop_size) / 2
+        mean_data = mean_data[:, crop_h_w:crop_h_w + crop_size, crop_h_w:crop_h_w + crop_size]
+
+        feature_unit = s.owl_net.units[s.owl_net.name_to_uid[s.layer_name][0]] 
+        batch_dir = 0
+        all_data = None
+        all_feature = None
+        for testiteridx in range(s.owl_net.solver.test_iter[0]):
+            print batch_dir
+            s.owl_net.forward('TEST')
+            feature = feature_unit.out.to_numpy()
+            if all_feature == None:
+                all_feature = feature
+            else:
+                all_feature = np.concatenate((all_feature, feature), axis=0)
+            data = data_unit.out.to_numpy()
+            if all_data == None:
+                all_data = data
+            else:
+                all_data = np.concatenate((all_data, data), axis=0)
+            batch_dir += 1
+            #HACK TODO: only take 10000 images
+            if np.shape(all_feature)[0] >= 10000:
+                break
+
+
+        #get the result 
+        feature_shape = feature_unit.out_shape
+        patch_shape = feature_unit.rec_on_ori
+        min_val = -float('inf') 
+        
+        #add back the mean file
+        for i in range(np.shape(all_data)[0]):
+            all_data[i,:,:,:] += mean_data
+       
+        if len(feature_shape) == 4:
+            #iter for each filter, for each filter, we choose nine patch from different image
+            for i in range(feature_shape[2]):
+                #create the result image for nine patches
+                res_img = np.zeros([feature_unit.rec_on_ori * 3, feature_unit.rec_on_ori * 3, 3])
+                filter_feature = np.copy(all_feature[:,i,:,:])
+                for patchidx in range(9):
+                    maxidx = np.argmax(filter_feature)
+                    colidx = maxidx % feature_shape[0]
+                    maxidx = (maxidx - colidx) / feature_shape[0]
+                    rowidx = maxidx % feature_shape[1]
+                    maxidx = (maxidx - rowidx) / feature_shape[1]
+                    imgidx = maxidx
+                    info = '%d %d %d' % (imgidx, rowidx, colidx)
+                    filter_feature[imgidx,:,:] = min_val
+                    
+                    #get the patch place
+                    patch_start_row = max(0,feature_unit.start_on_ori + rowidx * feature_unit.stride_on_ori)
+                    patch_end_row = min(feature_unit.start_on_ori + rowidx * feature_unit.stride_on_ori + feature_unit.rec_on_ori, data_unit.crop_size)
+
+                    patch_start_col = max(0,feature_unit.start_on_ori + colidx * feature_unit.stride_on_ori)
+                    patch_end_col = min(feature_unit.start_on_ori + colidx * feature_unit.stride_on_ori + feature_unit.rec_on_ori, data_unit.crop_size)
+                    patch = all_data[imgidx, :, patch_start_row:patch_end_row, patch_start_col:patch_end_col]
+
+                    #save img to image
+                    row_in_res = patchidx / 3
+                    col_in_res = patchidx % 3
+                    st_row = row_in_res * patch_shape 
+                    st_col = col_in_res * patch_shape
+                    #turn gbr into rgb
+                    res_img[st_row:st_row+patch_end_row - patch_start_row, st_col:st_col + patch_end_col - patch_start_col, 2] = patch[0,:,:]
+                    res_img[st_row:st_row+patch_end_row - patch_start_row, st_col:st_col + patch_end_col - patch_start_col, 1] = patch[1,:,:]
+                    res_img[st_row:st_row+patch_end_row - patch_start_row, st_col:st_col + patch_end_col - patch_start_col, 0] = patch[2,:,:]
+
+                #save img
+                res_img = Image.fromarray(res_img.astype(np.uint8))
+                res_path = '%s/%d.jpg' % (s.result_path, i)
+                print res_path
+                res_img.save(res_path, format = 'JPEG')
+
+        else:
+            assert(False)
+
+
+
+
+
+
+
+
+
+
