eac797e27d68b6962c1bf9dcf1827c0fa34793ed
Yutian Li <hotpxless@gmail.com>
2015-05-05 01:08:55 +0800

    [athena-cython] move files

--- athena/boot.py
+++
@@ -1,4 +0,0 @@
-from athena import *
-initialize()
-set_device(create_gpu_device(0))
-
--- setup.py
+++ setup.py
@@ -1,44 +1,46 @@
 #!/usr/bin/env python
-import os, sys
-import shutil
-import logging
 from setuptools import setup, Extension
-from setuptools.command.install import install
-from setuptools.command.build_ext import build_ext
+from Cython.Build import cythonize
+import os
 
-logging.getLogger().addHandler(logging.StreamHandler())
-logger = logging.getLogger()
-if not os.path.exists(os.path.realpath("owl/owl/libowl.so")):
-    logger.error("Fatal: libowl.so not built. Please build c++ library first.")
-    sys.exit(1)
-package_data = {'owl': ["libowl.so"]}
+# Hack to use specified compiler
+os.environ['CC'] = 'clang++'
+os.environ['OPT'] = ''
 
-def copy_lib(build_dir, lib_name):
-    """ copy libminerva.so and libowl.so into source dir
-    
-    This command is prior to running a bdist
-    """
-    root = os.path.dirname(__file__)
-    local = os.path.abspath(os.path.join(root, 'owl', 'owl'))
-    try:
-        lib = os.path.realpath(os.path.join(build_dir, 'lib', lib_name))
-        print ("copying %s -> %s" % (lib, local))
-        shutil.copy(lib, local)
-    except Exception:
-        if not os.path.exists(local):
-            logger.error("Fatal: local path not exist.")
-        if not os.path.exists(lib):
-            msg = ("%s not found. Please build c++ library first." % lib)
-            logger.error("Fatal: " + msg)
-
-setup(name='owl',
-    version='1.0',
-    maintainer='Minjie Wang',
-    maintainer_email='minerva-support@googlegroups.com',
-    license='Apache 2.0',
-    url='https://github.com/minerva-developers/minerva',
-    package_dir={'':'owl'},
-    packages=['owl', 'owl.net', 'owl.net.caffe'],
-    package_data=package_data,
-    zip_safe=False
+extensions = [
+    Extension(
+        '*',
+        ['owl/*.pyx', 'owl/minerva_utils.cpp'],
+        language='c++',
+        include_dirs=[
+            'minerva',
+            'third_party',
+            '/usr/local/cuda/include',
+            '/home/yutian/cpp/cudnn-6.5-linux-x64-v2'
+        ],
+        extra_compile_args=[
+            '-std=c++11',
+            '-Wall',
+            '-O2',
+            '-g'
+        ],
+        define_macros=[
+            ('HAS_CUDA', None),
+        ],
+        libraries=[
+            'minerva'
+        ],
+        library_dirs=[
+            'release/lib'
+        ],
+        runtime_library_dirs=[
+            'release/lib'
+        ],
     )
+]
+
+setup(
+    name='owl',
+    package_dir={'': 'owl'},
+    ext_modules=cythonize(extensions)
+)
