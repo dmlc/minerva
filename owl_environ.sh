73a380c8bb32f728614d019906f9a4d75e931911
Yutian Li <hotpxless@gmail.com>
2015-05-05 01:17:39 +0800

    [athena-cython] start script

--- owl_environ.sh
+++ owl_environ.sh
@@ -1,14 +1,8 @@
-called=$_
-if [[ $called != $0 ]]; then
-  if [[ -z ${OWL_ENABLED} ]]; then
-    SCRIPTPATH=$(dirname $(readlink -f "${BASH_SOURCE[@]}"))
-    export OWL_ENABLED=true
-    export PYTHONPATH=${SCRIPTPATH}/release/owl:${SCRIPTPATH}/owl:$PYTHONPATH
-    export PS1="(Owl Ready) ${PS1}"
-  else
-    echo "Owl already enabled"
-  fi
+if [[ -z ${OWL_ENABLED} ]]; then
+  SCRIPTPATH=$(dirname $(readlink -f $0))
+  export OWL_ENABLED=true
+  export PYTHONPATH=${SCRIPTPATH}/release/owl:${SCRIPTPATH}/owl:$PYTHONPATH
+  export PS1="(Owl Ready) ${PS1}"
 else
-  echo "Script $0 should be sourced, not run. Use:" 1>&2
-  echo "  . $0" 1>&2
+  echo "Owl already enabled"
 fi
