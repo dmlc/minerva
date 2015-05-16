dbf478f9e69421e5415a5a6dfe326822d5962911
Minjie Wang <wmjlyjemaine@gmail.com>
2015-05-16 16:41:31 -0400

    [athena-cython] get rid of cuda_runtime.h header dependency when include minerva.h

--- minerva/op/physical_fn.h
+++ minerva/op/physical_fn.h
@@ -1,13 +1,14 @@
 #pragma once
 #include "op/basic_fn.h"
 #include "op/physical.h"
-#include "op/context.h"
 #include "op/impl/impl.h"
 #include "common/common.h"
 #include "op/data_shard.h"
 #include "op/compute_fn.h"
 
 namespace minerva {
+
+struct Context;
 
 template<typename Closure>
 class ComputeFnWithClosure : public ComputeFn, public ClosureTrait<Closure> {
