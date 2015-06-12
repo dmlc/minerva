38ba4e42542b495fcfd7bf246d5b778737d05e56
Minjie Wang <wmjlyjemaine@gmail.com>
2015-06-11 22:07:31 -0400

    [master] more commands in clean

--- build.sh
+++ build.sh
@@ -14,7 +14,9 @@ function run_clean {
   if [ "$yesorno" == "y" ]; then
       echo "Removing builds"
       rm -rf $BUILD_DIR
+      python setup.py clean --all
       rm -rf build
+      rm -rf dist
       rm -rf owl/libowl.so
   fi
   exit 0
