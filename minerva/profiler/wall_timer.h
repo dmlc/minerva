1be43c0fc36da7ef3c60c15f10ba25d28fb7da0a
Cheng CHEN <popstarshine@163.com>
2015-06-29 17:40:07 +0800

    clean up the port project
    clean up the port project

--- minerva/profiler/wall_timer.h
+++ minerva/profiler/wall_timer.h
@@ -4,20 +4,19 @@
 # include <winsock.h>
 inline int gettimeofday(struct timeval* p, void* tz /* IGNORED */)
 {
-    union {
-        long long ns100; /*time since 1 Jan 1601 in 100ns units */
-        FILETIME ft;
-    } now;
+  union {
+    long long ns100; /*time since 1 Jan 1601 in 100ns units */
+     FILETIME ft;
+  } now;
 
-    GetSystemTimeAsFileTime(&(now.ft));
-    p->tv_usec = (long)((now.ns100 / 10LL) % 1000000LL);
-    p->tv_sec = (long)((now.ns100 - (116444736000000000LL)) / 10000000LL);
-    return 0;
+  GetSystemTimeAsFileTime(&(now.ft));
+  p->tv_usec = (long)((now.ns100 / 10LL) % 1000000LL);
+  p->tv_sec = (long)((now.ns100 - (116444736000000000LL)) / 10000000LL);
+  return 0;
 }
 
 # else
 #include <sys/time.h>
-
 # endif
 
 #include "profiler/timer.h"
@@ -25,21 +24,21 @@ inline int gettimeofday(struct timeval* p, void* tz /* IGNORED */)
 
 namespace minerva {
 
-    class WallTimer : public Timer {
-    public:
-        WallTimer();
-        WallTimer(const WallTimer&);
-        WallTimer& operator=(const WallTimer&);
-        ~WallTimer();
-        void Start();
-        void Stop();
-        double StartTimeMicrosecond() const;
-        double EndTimeMicrosecond() const;
-        double ReadMicrosecond() const;
+class WallTimer : public Timer {
+public:
+  WallTimer();
+  WallTimer(const WallTimer&);
+  WallTimer& operator=(const WallTimer&);
+  ~WallTimer();
+  void Start();
+  void Stop();
+  double StartTimeMicrosecond() const;
+  double EndTimeMicrosecond() const;
+  double ReadMicrosecond() const;
 
-    private:
-        timeval start_;
-        timeval end_;
-    };
+private:
+  timeval start_;
+  timeval end_;
+};
 
 }  // namespace minerva
